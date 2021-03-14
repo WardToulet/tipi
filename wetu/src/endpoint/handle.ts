05232657c0553c519207a0c21fedb967fe5f3dca
Ward Toulet <ward@toulet.net>
2021-03-14 20:51:34 +0100

    [doc] Added some documentation

--- wetu/src/endpoint/handle.ts
+++ wetu/src/endpoint/handle.ts
@@ -1,7 +1,13 @@
 import Request from './request';
 
 /**
- * A function to incapsulate all the bussinis logic of an endopint
+ * Handlefunc contains all the bussines logic needed for an endpoint, 
+ * it is the function that is eventually executed when the endpoint gets called.
+ *
+ * @param req {Request} Incapsulates the request made by the client
+ * @throws If a HandleFunc throws the request is canceled an an error code gets send to the client
+ * @returns {Promise<ResBody>} The data that can be send to the client that made the request (unmarchalled)
+ *
  */
 export default interface HandleFunc<ReqBody, URLParameters, QueryParameters, ResBody, Context> {
   (req: Request<ReqBody, URLParameters, QueryParameters, Context>): Promise<ResBody>
--- wetu/src/endpoint/middleware.ts
+++ wetu/src/endpoint/middleware.ts
@@ -1,5 +1,13 @@
 import Request from './request';
 
+/**
+ * MiddlewareFunc gets executed beffor the HandleFunc gets executed. 
+ * The can be used to perform checks, or add data to the context.
+ *
+ * @param req {Request} incapluated client request
+ * @throws When a MiddlewareFunc throws the request is terminated and an apporiate
+ * statuscode is send to the client.
+ */
 export default interface MiddlewareFunc<ReqBody, URLParameters, QueryParameters> {
   (req: Request<ReqBody, URLParameters, QueryParameters>): Promise<Request<ReqBody, URLParameters, QueryParameters>>
 }
